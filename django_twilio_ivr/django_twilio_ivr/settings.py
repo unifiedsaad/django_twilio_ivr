b82758c4f1c11d66c4a66ce81cb7a2ad1742e274
Saad Mirza <44167576+unifiedsaad@users.noreply.github.com>
2019-02-02 00:11:03 +0500

    remove slashes for heroku

--- django_twilio_ivr/django_twilio_ivr/settings.py
+++ django_twilio_ivr/django_twilio_ivr/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
-MEDIA_URL = "/media/"
+STATIC_URL = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = os.path.join(BASE_DIR, 'media')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
