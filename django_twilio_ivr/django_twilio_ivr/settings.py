f2d90d9c7a62bea59158482b4538dd3ef64922ac
unifiedsaad <saad.m@whyunified.com>
2019-02-02 01:49:48 +0500

    updates

--- django_twilio_ivr/django_twilio_ivr/settings.py
+++ django_twilio_ivr/django_twilio_ivr/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
