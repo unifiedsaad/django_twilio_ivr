5b89fbea50adbd6d442fa2abd604203d6a08b251
Saad Mirza <44167576+unifiedsaad@users.noreply.github.com>
2019-02-02 00:15:22 +0500

    changes for heroku

--- django_twilio_ivr/django_twilio_ivr/settings.py
+++ django_twilio_ivr/django_twilio_ivr/settings.py
@@ -116,7 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_URL = "media/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
