d1ceb3d0b9377a9cf44bf89b06a7af85a94b4768
Saad Mirza <44167576+unifiedsaad@users.noreply.github.com>
2019-02-02 00:12:26 +0500

    add slash for heroku

--- django_twilio_ivr/django_twilio_ivr/settings.py
+++ django_twilio_ivr/django_twilio_ivr/settings.py
@@ -118,5 +118,5 @@ USE_TZ = True
 
 STATIC_URL = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_URL = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
