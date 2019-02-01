0283deda54419afffe9ebf9d8c10cd3dd9c1fa51
unifiedsaad <saad.m@whyunified.com>
2019-02-02 01:47:11 +0500

    disable static

--- django_twilio_ivr/django_twilio_ivr/settings.py
+++ django_twilio_ivr/django_twilio_ivr/settings.py
@@ -115,14 +115,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-DISABLE_COLLECTSTATIC=1
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
