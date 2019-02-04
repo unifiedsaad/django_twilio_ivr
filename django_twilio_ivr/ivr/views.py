dea1e9ed1e0401be3daa67148d96a0a6fd96c0f5
unifiedsaad <saad.m@whyunified.com>
2019-02-05 00:12:14 +0500

    update some code

--- django_twilio_ivr/ivr/views.py
+++ django_twilio_ivr/ivr/views.py
@@ -4,9 +4,17 @@ from django.http import HttpResponse
 
 def init_automated_call(request):
     """ initial end point called by twilio on making a phone call to your twilio number"""
-
     response = VoiceResponse()
-    response.say('Hello World')
+    voice_message = 'Hi its Saad Mirza Press one to hear a monkey, two to receive an SMS'
+
+    with response.gather(action='/respond/', num_digits=1) as g:
+        g.say(voice_message)
+        g.pause(length=1)
+
+    # if user does not response with an option
+    # call your http://{domain}/gather end point again to repeat options
+    response.redirect('/gather/')
+
     # HttResponse will return xml response object for twilio api to process
     return HttpResponse(str(response), content_type='application/xml')
 
@@ -20,11 +28,12 @@ def handle_user_response(request):
 
     # evaluate user input from phones keypad and take an appropriate action
     if digits == '1':
+        response.say('you are going to listen a very beautiful song, so hang tight.')
         response.play('http://demo.twilio.com/hellomonkey/monkey.mp3')
     if digits == '2':
         number = request.POST.get('From', '')
-        response.say('Thank you for calling, for more content see site blog')
-        response.sms('Thanks for trying out this tutorial, share with your friends!', to=number)
+        response.say('Thank you for Receiving calling, i thought you sleeping')
+
 
     # HttResponse will return xml response object for twilio api to process
     return HttpResponse(str(response), content_type='application/xml')
